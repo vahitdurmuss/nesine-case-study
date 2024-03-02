97afe6b190a7b839e636547ea03ed394eba966ea
INNOVA\vdurmus <vdurmus@innova.com.tr>
2024-03-02 15:29:05 +0300

    build and jdk configuration

---
+++ app/src/main/java/com/nesine/casestudy/ui/CaseStudyActivity.kt
@@ -0,0 +1,12 @@
+package com.nesine.casestudy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nesine.casestudy.R
+
+class CaseStudyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_case_study)
+    }
+}
