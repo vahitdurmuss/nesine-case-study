a9e31b3700933546321fc8329fed8e4302532cef
INNOVA\vdurmus <vdurmus@innova.com.tr>
2024-03-02 21:53:12 +0300

    create list uı, build viewmodel, collect states of ui in list fragment

--- app/src/main/java/com/nesine/casestudy/ui/list/ListFragment.kt
+++ app/src/main/java/com/nesine/casestudy/ui/list/ListFragment.kt
@@ -6,27 +6,77 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
 import com.nesine.casestudy.R
+import com.nesine.casestudy.databinding.FragmentListBinding
+import com.nesine.casestudy.ui.common.UIResult
+import kotlinx.coroutines.launch
 
 class ListFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ListFragment()
-    }
-
     private lateinit var viewModel: ListViewModel
+    private lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding= FragmentListBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm=viewModel
+        observeStates()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
-        // TODO: Use the ViewModel
+    }
+
+    private fun observeStates() {
+
+        lifecycleScope.launch {
+
+            viewModel.postStateFlow.collect {
+
+                when(it){
+
+                    is UIResult.Success-> {
+
+                    }
+
+                    is UIResult.Failure->{
+
+                    }
+
+                    is UIResult.GeneralFailure->{
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        lifecycleScope.launch {
+
+            viewModel.uiStatusStateFlow.collect {
+               when(it){
+                   ListViewModel.UIStatus.Loading->{
+
+                   }
+                   ListViewModel.UIStatus.Done->{
+
+                   }
+               }
+            }
+
+        }
     }
 
 }
--- app/src/main/java/com/nesine/casestudy/ui/list/ListViewModel.kt
+++ app/src/main/java/com/nesine/casestudy/ui/list/ListViewModel.kt
@@ -1,7 +1,53 @@
 package com.nesine.casestudy.ui.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nesine.casestudy.ui.common.UIResult
+import com.nesine.casestudy.ui.core.data.PostModel
+import com.nesine.casestudy.ui.core.data.PostRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class ListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ListViewModel(postRepository: PostRepository) : ViewModel() {
+
+    private val _postsStateFlow = MutableStateFlow<UIResult<List<PostModel>?>>(
+        UIResult.Success(
+            emptyList()
+        )
+    )
+    val postStateFlow: StateFlow<UIResult<List<PostModel>?>> = _postsStateFlow
+
+    private val _uiStatusStateFlow = MutableStateFlow<UIStatus>(UIStatus.Loading)
+    val uiStatusStateFlow: StateFlow<UIStatus> = _uiStatusStateFlow
+
+    sealed class UIStatus {
+        object Loading : UIStatus()
+        object Done : UIStatus()
+    }
+
+    init {
+
+        viewModelScope.launch {
+
+            when (val result = postRepository.getPosts()) {
+                is UIResult.Success -> {
+                    _postsStateFlow.value = result
+                }
+
+                is UIResult.Failure -> {
+                    _postsStateFlow.value = result
+                }
+
+                is UIResult.GeneralFailure -> {
+                    _postsStateFlow.value = result
+                }
+
+            }
+
+
+        }
+
+    }
+
 }
